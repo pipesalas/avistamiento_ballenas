d5c8ae7ff38600ff5c7fcb76694757a1f29eb220
Felipe Salas <felipe.salas.b@gmail.com>
2023-02-28 17:12:11 -0300

    delete old pages

--- app/🔭_Ocurrencia_de_avistamientos.py
+++ app/🔭_Ocurrencia_de_avistamientos.py
@@ -21,7 +21,7 @@ def main():
     }
     )
 
-    st.title('Visualizador de las condiciones marítimas :ocean:')
+    st.title('Visualizador de las condiciones marítimas :ocean: :whale:')
 
     st.caption('''Para visualizar las condiciones marinas usamos la integración de ee BLABLA y agrupamos por cuadrados, sacamos la MEDIANA del mes, blabla''' )
 
@@ -47,21 +47,11 @@ def main():
         
 
     # Create Streamlit app
-    st.title('Whale Occurrences in Chile :whale:')
     st.write('''¡Bienvenido a nuestra aplicación Ocurrencias de Ballenas en Chile! 
     Esta aplicación le permite explorar las ocurrencias de especies de ballenas registradas en el país de Chile, utilizando datos de Global Biodiversity Information Facility (GBIF). 
     Con esta aplicación, puede ver una tabla de datos de ocurrencia para cada especie de ballena, así como un mapa de las ubicaciones donde se han observado estas especies. 
     Puede acercar y alejar el mapa y hacer clic en marcadores individuales para ver más información sobre cada ocurrencia. 
     Esta aplicación es una gran herramienta para científicos, educadores y cualquier persona interesada en aprender más sobre la distribución de las especies de ballenas en Chile.''')
-
-
-
-
-    
-    
-    
-   
-    st.subheader(f'Occurrences Map for {whale_species_selection} :whale2:')
 
 
     # cargamos datos
@@ -72,7 +62,7 @@ def main():
     
 
 
-    st.header('Clorofila en la superficie del oceano 🍀')
+    st.subheader(f'Clorofila en la superficie del oceano 🍀, ballena {whale_species_selection}')
     plot_chlorophyll(gdf_chlor, gdf_ballenas)
     st.caption('''Se sabe que las ballenas se alimentan de fitoplancton, que son plantas microscópicas que viven en el océano. 
     Estas plantas dependen de la luz solar y los nutrientes para crecer y, como resultado, su abundancia a menudo está relacionada con la concentración de clorofila en el agua. 
@@ -81,7 +71,7 @@ def main():
     En otras palabras, las áreas con altas concentraciones de clorofila pueden ser buenos lugares de alimentación para las ballenas, y monitorear los niveles de clorofila puede ayudarnos
     a comprender mejor el comportamiento y la distribución de las ballenas.''')
     
-    st.header('Temperatura en la superficie del oceano 🌡')
+    st.subheader(f'Temperatura en la superficie del oceano 🌡, ballena {whale_species_selection}')
     plot_temperature(gdf_temp, gdf_ballenas)
     st.caption('''La relación entre la presencia de fitoplancton y la temperatura del mar es compleja y, a menudo, depende de una variedad de factores. 
     Generalmente, el fitoplancton prospera en aguas más cálidas, pero hay muchos otros factores que entran en juego, como la disponibilidad de nutrientes, los niveles de luz y el movimiento del agua. 
@@ -118,37 +108,6 @@ def main():
 
 
     
-
-@st.cache_data
-def load_temperature_geodataframe(mes: str) -> gpd.GeoDataFrame:
-    gdf_temp = gpd.read_file(f'app/data/temperature_polygons_{mes}.json')
-    gdf_temp = gdf_temp.set_crs('epsg:4326')
-    gdf_temp['text'] = gdf_temp.temp.apply(lambda x: f"<b>Temperature :</b> {x: .1f} ºC")
-    return gdf_temp
-    
-
-@st.cache_data
-def load_chlorophyll_geodataframe(mes: str) -> gpd.GeoDataFrame:
-    gdf_temp = gpd.read_file(f'app/data/chlorophyll_polygons_{mes}.json')
-    gdf_temp = gdf_temp.set_crs('epsg:4326')
-    gdf_temp['text'] = gdf_temp.chlor_raw.apply(lambda x: f"<b>Chlorophyll:</b> {x} mg/m³")
-    return gdf_temp
-
-
-@st.cache_data
-def load_ruta() -> gpd.GeoDataFrame:
-    ruta = (gpd.read_file('app/data/Track_OOO.gpx', layer='tracks')).explode().reset_index(drop=True)
-    ruta = ruta.set_crs('epsg:4326')
-    ruta['text'] = ruta.name.apply(lambda x: f'ruta {x}')
-    return ruta
-
-
-@st.cache_data    
-def define_viewstate() -> pdk.ViewState: 
-    return pdk.ViewState(
-        **{"latitude": -39.96, "longitude": -73.7, "zoom": 10, "maxZoom": 13, "minZoom": 7, "pitch": 20, "bearing": 0}
-    )
-
 
 
 
@@ -191,7 +150,6 @@ def plot_chlorophyll(gdf, gdf_ballenas):
         wireframe=True,
     )
 
-    
 
     ruta = pdk.Layer(
                     'PathLayer',
@@ -301,7 +259,6 @@ def plot_temperature(gdf_temp, gdf_ballenas):
         pickable=True,
     )
    
-
     ruta = pdk.Layer(
         'PathLayer',
         data=ruta,
@@ -339,8 +296,35 @@ def plot_temperature(gdf_temp, gdf_ballenas):
     st.pydeck_chart(r)
 
 
+@st.cache_data
+def load_temperature_geodataframe(mes: str) -> gpd.GeoDataFrame:
+    gdf_temp = gpd.read_file(f'app/data/temperature_polygons_{mes}.json')
+    gdf_temp = gdf_temp.set_crs('epsg:4326')
+    gdf_temp['text'] = gdf_temp.temp.apply(lambda x: f"<b>Temperature :</b> {x: .1f} ºC")
+    return gdf_temp
+    
+
+@st.cache_data
+def load_chlorophyll_geodataframe(mes: str) -> gpd.GeoDataFrame:
+    gdf_temp = gpd.read_file(f'app/data/chlorophyll_polygons_{mes}.json')
+    gdf_temp = gdf_temp.set_crs('epsg:4326')
+    gdf_temp['text'] = gdf_temp.chlor_raw.apply(lambda x: f"<b>Chlorophyll:</b> {x} mg/m³")
+    return gdf_temp
 
 
+@st.cache_data
+def load_ruta() -> gpd.GeoDataFrame:
+    ruta = (gpd.read_file('app/data/Track_OOO.gpx', layer='tracks')).explode().reset_index(drop=True)
+    ruta = ruta.set_crs('epsg:4326')
+    ruta['text'] = ruta.name.apply(lambda x: f'ruta {x}')
+    return ruta
+
+
+@st.cache_data    
+def define_viewstate() -> pdk.ViewState: 
+    return pdk.ViewState(
+        **{"latitude": -39.96, "longitude": -73.7, "zoom": 10, "maxZoom": 13, "minZoom": 7, "pitch": 20, "bearing": 0}
+    )
 
 
 @st.cache_data
@@ -380,7 +364,7 @@ def request_gbif_api(taxonkey: int = 2440735):
 
     df = df.dropna()
     gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['decimalLongitude'], df['decimalLatitude']), crs='EPSG:4326')
-    gdf['text'] = gdf.apply(lambda x: x.species + ' ' + x.eventDate[:10], axis=1)
+    gdf['text'] = gdf.apply(lambda x: x.species + '<br>' + x.eventDate[:10], axis=1)
     return gdf
 
 
