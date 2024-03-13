2f2fa11c3c2b6f52d728ba97a8e6f6ec7dab3a3c
Felipe Salas <felipe.salas.b@gmail.com>
2024-03-13 13:48:17 -0300

    primera versión de app funcionando

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ from netCDF4 import Dataset
 def main():
 
     st.set_page_config(
-    page_title="Condiciones maritimas",
+    page_title="Avistamiento mamíferos",
     page_icon="🌊",
     layout="wide",
     menu_items={
@@ -30,126 +30,148 @@ def main():
 
 
     
-
+    lat_min, lat_max = -41, -39
     # cargamos datos
     df_avistamientos = load_datos_avistamientos()
     ruta = load_ruta()
     chlorophyll = load_chlorophyll()
-    temperature = load_temperature()
+    temperature = load_temperature(lat_min, lat_max)
+
+    _, col_mapa, _ = st.columns([1, 5, 1])
+    with col_mapa:
+        with st.expander('Datos brutos', expanded=False):
+            st.write(df_avistamientos.head())
+            st.write(chlorophyll.head())
+            st.write(temperature.head())
+
+    
+        st.header('Mapa de avistamientos')
+        plot_mapa(temperature, ruta, df_avistamientos)
 
 
-    with st.expander('Datos brutos', expanded=True):
-        st.write(df_avistamientos.head())
-        st.write(chlorophyll.head())
-        st.write(temperature.query('time<"2023-01-02"').sample(15))
-        st.write(temperature.dtypes)
 
+
+def plot_mapa(temperature, ruta, df_avistamientos):
+    color_range = [
+        [65, 182, 196],
+        [127, 205, 187],
+        [199, 233, 180],
+        [237, 248, 177],
+        [255, 255, 204],
+        [255, 237, 160],
+        [254, 217, 118],
+        [254, 178, 76],
+        [253, 141, 60],
+        [252, 78, 42],
+        [227, 26, 28],
+        [189, 0, 38],
+        [128, 0, 38],
+    ]
     # Create bins for temperature
-    bins = np.linspace(temperature['temperature'].min(), temperature['temperature'].max(), 10)
-    labels = range(1, len(bins))
-    temperature['temp_bin'] = pd.cut(temperature['temperature'], bins=bins, labels=labels)
+    bins = np.linspace(temperature['temperature'].min(), temperature['temperature'].max(), len(color_range))
 
-    st.pydeck_chart(pdk.Deck(
-        map_style=None,
-        initial_view_state=pdk.ViewState(
-            latitude=-39.5,
-            longitude=-73.5,
-            zoom=8,
-            pitch=50,
+    def color_scale(val):
+        for i, b in enumerate(bins):
+            if val < b:
+                return color_range[i]
+        return color_range[i]
+
+    temperature["fill_color"] = temperature["temperature"].apply(lambda row: color_scale(row))
+
+    layers = [
+        pdk.Layer(
+            'PolygonLayer',
+            data=temperature.query('time<"2023-01-02"')[['longitude', 'latitude', 'geometry', 'temperature', 'fill_color']],
+            get_polygon='geometry.coordinates',
+            get_elevation='temperature',
+            get_fill_color='fill_color',
+            get_line_color=[128, 128, 128], 
+            line_width_min_pixels=2,
+            opacity=0.5,
+            pickable=True,
+            stroked=False,
+            auto_highlight=True,
+            tooltip={"text": "Temperatura: {temperature} °C"},
         ),
-        layers=[
-            # pdk.Layer(
-            # 'HeatmapLayer',
-            # data=temperature.query('time<"2023-01-02"')[['lon','lat', 'temperature']],
-            # get_position='[lon, lat]',
-            # get_weight='temperature',
-            # opacity=0.1,
-            # threshold=temperature.query('time<"2023-01-02"').temperature.mean(),
-            # intensity=10,
-            # aggregation='"MEAN"',
-            # ),
-            pdk.Layer(
-                'PolygonLayer',
-                data=temperature.query('time<"2023-01-02"')[['lon', 'lat', 'geometry', 'temperature']],
-                get_polygon='geometry.coordinates',
-                get_elevation='temperature',
-                get_fill_color=[180, 0, 200, 140],
-                get_line_color=[255, 255, 255],
-                line_width_min_pixels=2,
-                opacity=0.5,
-            ),
-        ],
-    ))
-    # Set the initial view
-    # view_state = pdk.ViewState(
-    #     latitude=df_avistamientos['lat'].mean(),
-    #     longitude=df_avistamientos['lon'].mean(),
-    #     zoom=6,
-    #     min_zoom=5,
-    #     max_zoom=15,
-    #     pitch=40.5,
-    #     bearing=-27.36)
+        pdk.Layer(
+            'TextLayer',
+            data=temperature.query('time<"2023-01-02"')[['longitude', 'latitude', 'geometry', 'temperature_str', 'fill_color']],
+            get_position=[ 'longitude', 'latitude'],
+            get_text='temperature_str',
+            get_color=[128,128,128],
+            get_size=10,
+            get_alignment_baseline="'bottom'",
+        ),
+        pdk.Layer(
+            'PathLayer',
+            data=ruta,
+            get_path='geometry.coordinates',
+            get_color=[128, 128, 128],  # Changed to gray color
+            width_scale=20,
+            width_min_pixels=6,
+            get_width='width',
+            pickable=False,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=df_avistamientos[['lat', 'lon', 'Especie', 'color']].copy(),
+            get_position=['lon', 'lat'],
+            get_color='color',  
+            get_radius=200,
+            pickable=True,
+            auto_highlight=True,
+            #tooltip=,
+        )
+    ]
 
-    # # Create a layer for temperature heatmap
-    # temperature_layer = pdk.Layer(
-    #     'HeatmapLayer',
-    #     data=temperature,
-    #     opacity=0.9,
-    #     get_position=['longitude', 'latitude'],
-    #     threshold=0.3,
-    #     get_weight='temperature',
-    #     aggregation='"MEAN"'
-    # )
+    view_state = pdk.ViewState(
+        latitude=-39.5,
+        longitude=-73.8,
+        zoom=8,
+        pitch=50,
+    )
 
-    # # Create a layer for sightings
-    # sightings_layer = pdk.Layer(
-    #     'ScatterplotLayer',
-    #     data=df_avistamientos,
-    #     get_position=['lon', 'lat'],
-    #     get_color=[200, 30, 0, 160],
-    #     get_radius=200,
-    # )
-
-    # # Combine all layers
-    # layers = [temperature_layer, sightings_layer]
-
-    # # Render the map
-    # st.pydeck_chart(pdk.Deck(
-    #     map_style='mapbox://styles/mapbox/light-v9',
-    #     initial_view_state=view_state,
-    #     layers=layers,
-    # ))
+    deck = pdk.Deck(
+        map_style=pdk.map_styles.LIGHT,
+        initial_view_state=view_state,
+        layers=layers,
+        tooltip={"text": "Especie: {Especie}"}
+    )
+    st.pydeck_chart(deck)
+    
+    
+def crop_map(df, lat_min, lat_max, variable='temperature'):
+    min_depth = df.depth.min()
+    df = df.query(f'latitude >= {lat_min} and latitude <= {lat_max} and depth == @min_depth')
+    
+    df = df.dropna(how='any')
+    return df
 
 
-@st.cache_data()
-def load_temperature():
-
-    temperature = pd.read_pickle('app/data/temperature.pkl')
-    min_depth = temperature.depth.min()
-    temperature = temperature.query('temperature > 0 and depth == @min_depth')
-    temperature['temperature'] = temperature['temperature'].astype(float)
-    temperature.rename(columns={'latitude': 'lat', 'longitude': 'lon'}, inplace=True)
-    temperature.dropna(how='any', inplace=True)
-    # Define the grid size
-    grid_size = 0.08
-
-    # Create a square geometry around each coordinate
-    temperature['geometry'] = temperature.apply(lambda row: MultiPoint([(row['lon'] - grid_size / 2, row['lat'] - grid_size / 2), 
-                                                                        (row['lon'] + grid_size / 2, row['lat'] - grid_size / 2), 
-                                                                        (row['lon'] + grid_size / 2, row['lat'] + grid_size / 2), 
-                                                                        (row['lon'] - grid_size / 2, row['lat'] + grid_size / 2)]).convex_hull, axis=1)
-
-    # Convert the DataFrame to a GeoDataFrame
+def create_grid(temperature, grid_size = 0.08):
+    
+    temperature['geometry'] = temperature.apply(lambda row: MultiPoint([(row['longitude'] - grid_size / 2, row['latitude'] - grid_size / 2), 
+                                                                        (row['longitude'] + grid_size / 2, row['latitude'] - grid_size / 2), 
+                                                                        (row['longitude'] + grid_size / 2, row['latitude'] + grid_size / 2), 
+                                                                        (row['longitude'] - grid_size / 2, row['latitude'] + grid_size / 2)]).convex_hull, axis=1)
     temperature = gpd.GeoDataFrame(temperature, geometry='geometry')
     return temperature
 
 
 @st.cache_data()
+def load_temperature(lat_min, lat_max):
+
+    temperature = pd.read_pickle('app/data/temperature.pkl')
+    temperature = temperature.pipe(crop_map, lat_min, lat_max).pipe(create_grid, grid_size=0.08)
+    temperature['temperature'] = temperature['temperature'].round(1)
+    temperature['temperature_str'] = temperature['temperature'].astype(str) + ' °C'
+    return temperature
+
+@st.cache_data()
 def load_chlorophyll():
     
     chlorophyll = pd.read_pickle('app/data/chlorophyll.pkl')
-    min_depth = chlorophyll.depth.min()
-    chlorophyll = chlorophyll.query('chlorophyll > 0 and depth == @min_depth')
+    #chlorophyll = chlorophyll.pipe(crop_map, -40, -39, variable='chlorophyll').pipe(create_grid, grid_size=0.2)
     return chlorophyll
 
 
@@ -165,6 +187,10 @@ def load_ruta() -> gpd.GeoDataFrame:
 @st.cache_data()
 def load_datos_avistamientos():
     df = pd.read_excel('app/data/datos_avistamientos.xlsx')
+    unique_species = df['Especie'].unique()
+    colors = [[np.random.randint(0, 256), np.random.randint(0, 256), np.random.randint(0, 256)] for _ in range(len(unique_species))]
+    species_color = dict(zip(unique_species, colors))
+    df['color'] = df['Especie'].map(species_color)
     return df
 
 
